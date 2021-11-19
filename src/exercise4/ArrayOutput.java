04ff79aa1844439152e7bd8b3da27121758bdfd8
AlexanderYkmv <>
2021-11-19 10:41:06 +0000

    a

--- src/exercise4/ArrayOutput.java
+++ src/exercise4/ArrayOutput.java
@@ -13,15 +13,17 @@ public class ArrayOutput {
 
     int size = input.nextInt();
     int[] array = new int[size];
-    for(int i = 0; i < array.length; i++) {
-      array[i] = input.nextInt(); 
-    }
     for(int index = 0; index < array.length; index++) {
+      array[index] = input.nextInt(); 
+    }
+    for(int i = 0; i < array.length; i++) {
       for(int j = 0; j < array.length; j++) {
         System.out.print(" ");
       }
-     System.out.print(array[index]);
+       System.out.println(array[i]);
+  
     }
+   
 
 
   }
